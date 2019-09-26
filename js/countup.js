1e739d271b60fb871ebde161b6d4853b7f69e3df
t-tm <ckqpw101.work@gmail.com>
2019-09-26 18:00:07 +0900

    add variable definition

--- js/countup.js
+++ js/countup.js
@@ -1,11 +1,12 @@
 // JavaScript Document
 $(window).on('load' , function(){
   var countupNum = 0;
+  var maxNum = 99;//いくつまでカウントアップするか
   //【※注】変数にsetIntervalを入れて、その変数名に対してclearIntervalを設定しないと止まらない。
   var numTimer = setInterval (function(){
     $(".count").text( countupNum );
     countupNum++;
-    if( countupNum > 99 ){
+    if( countupNum > maxNum ){
       clearInterval(numTimer);
     }
   },10);
